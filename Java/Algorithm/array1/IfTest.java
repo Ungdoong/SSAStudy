f5e2ae3f362ef4cc0016ee4f76f823d2b3a6e968
TaekjinJeong <41600558+Ungdoong@users.noreply.github.com>
2019-07-15 19:27:17 +0900

    Add files via upload

---
+++ Java/Algorithm/array1/IfTest.java
@@ -0,0 +1,23 @@
+package array1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class IfTest {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int T=sc.nextInt();
+		
+		Random r = new Random();
+		int T;
+		int i = 0;
+		
+		while(i++ < 100) {
+			T = r.nextInt(100)+1;
+			if (T%2 == 0)
+				System.out.println(T + " : 짝수");
+			else
+				System.out.println(T + " : 홀수");
+		}
+	}
+}
